b9d1823e7133975885978cc9185d5cf269a2c182
EdmundoD3 <102501049+EdmundoD3@users.noreply.github.com>
2023-04-06 01:46:44 -0600

    refactorize Notes y User de models

--- src/models/Notes.js
+++ src/models/Notes.js
@@ -1,54 +1,38 @@
+const {note:table} = require('../sql/tables')
+//sqlite database
+const database = require("../db/database")
+
 const Notes = {
   get: async ({limit=10,offset=0})=> {
     //SQLite code
-  const {note:table} = require('../sql/tables')
   const sqliteCode = `SELECT * FROM notes ORDER BY id LIMIT ${limit} OFFSET ${offset}`
-    //sqlite database
-  const {get:databaseGet} = require("../db/database")
-  return await databaseGet({table:table,sqliteCode:sqliteCode})
+  return await database.get({table:table,sqliteCode:sqliteCode})
     },
   getById: async ({id=0})=> {
   //SQLite code
-  const {note:table} = require('../sql/tables')
   const sqliteCode = `SELECT * FROM notes WHERE id = '${id}'`
-  //sqlite database
-  const {get:databaseGet} = require("../db/database")
-  return await databaseGet({table:table,sqliteCode:sqliteCode});
+  return await database.get({table:table,sqliteCode:sqliteCode});
   },
   getByTitle: async ({title=''})=> {
     //SQLite code
-    const sqlWhere = `$title LIKE '${title}%'`
-    const {note:table} = require('../sql/tables')
-    const sqliteCode = `SELECT * FROM notes WHERE ${sqlWhere}`
-    //sqlite database
-    const {get:databaseGet} = require("../db/database")
-    return await databaseGet({table:table,sqliteCode:sqliteCode});
+    const sqliteCode = `SELECT * FROM notes WHERE title LIKE '${title}%'`
+    return await database.get({table:table,sqliteCode:sqliteCode});
   },
   post:async ({title, content})=> {
     //SQLite code
-    const {note:table} = require('../sql/tables')
     const insert = `INSERT INTO notes (title, content) VALUES ('${title}', '${content}')`
-    //sqlite database
-    const {post:databasePost} = require("../db/database")
-    return await databasePost({table:table,insert:insert,tableName:'notes'});
+    return await database.post({table:table,insert:insert,tableName:'notes'});
   },
   put:async ({id, title, content})=> {
     //SQLite code
-  const {note:table} = require('../sql/tables')
   const update = updateSqlData({id,title,content})
-
   const sqliteCode = `SELECT * FROM notes WHERE id = ${id}`
-  //sqlite database
-  const {put:databasePut} = require("../db/database")
-  return await databasePut({table:table,update:update,sqliteCode:sqliteCode});
+  return await database.put({table:table,update:update,sqliteCode:sqliteCode});
   },
   deleteById:async ({id})=> {
   //SQLite code
-  const {note:table} = require('../sql/tables')
   const sqliteDelete = `DELETE FROM notes WHERE id = ${id}`
-  //sqlite database
-  const {delete:databaseDelete} = require("../db/database")
-  return await databaseDelete({table:table,sqliteDelete:sqliteDelete});
+  return await database.delete({table:table,sqliteDelete:sqliteDelete});
   },
 }
 module.exports = Notes
@@ -57,5 +41,5 @@ const updateSqlData=({id,title,content}) =>{
   const updateTitle = !title? "":`title = '${title}'${!content?"":","}`
   const updateContent = !content?"":`content = '${content}'`
   const update = `UPDATE notes SET ${updateTitle} ${updateContent} WHERE id = ${id}`
-return update
+  return update
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,32 +1,23 @@
+const {user:table} = require('../sql/tables')
+const database = require('../db/database')
+
 const User = {
   get:  async ({limit=10,offset=0})=>{
     const sqliteCode = `SELECT * FROM notes ORDER BY id LIMIT ${limit} OFFSET ${offset}`
-    const {get:databaseGet} = require('../db/database')
-    const {user:table} = require('../sql/tables')
-    return await databaseGet({table,sqliteCode})
+    return await database.get({table,sqliteCode})
   },
   post:  async ({name='',email='',password=''})=>{
   const insert = `INSERT INTO user (name, email, password)
-  VALUES ('${name}', 
-  '${email}', 
-  '${password}')`
-    const {post:databasePost} = require('../db/database')
-    const {user:table} = require('../sql/tables')
-    return await databasePost({table,insert,tableName:'user'})
+  VALUES ('${name}', '${email}', '${password}')`
+    return await database.post({table,insert,tableName:'user'})
   },
   getById: async ({id=''})=>{
-    const {get:databaseGet} = require('../db/database')
     const sqliteCode = `SELECT * FROM user WHERE id = '${id}'`
-    const {user:table} = require('../sql/tables')
-
-    return await databaseGet({table:table,sqliteCode:sqliteCode});
+    return await database.get({table:table,sqliteCode:sqliteCode});
   },
   getByEmail: async ({email=''})=>{
-    const {get:databaseGet} = require('../db/database')
     const sqliteCode = `SELECT * FROM user WHERE email = '${email}'`
-    const {user:table} = require('../sql/tables')
-
-    return await databaseGet({table:table,sqliteCode:sqliteCode});
+    return await database.get({table:table,sqliteCode:sqliteCode});
   }
 }
 
